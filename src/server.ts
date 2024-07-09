74f2f47f8a784100a9c27250b045adbca99a98b9
elias-mb3 <monteiroelias783@gmail.com>
2024-07-09 15:37:27 -0300

    feat: finish create trip route

--- src/server.ts
+++ src/server.ts
@@ -1,21 +1,30 @@
 import fastify from "fastify";
-import { prisma } from "./lib/prisma.js";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import cors from "@fastify/cors";
+import { createTrip } from "../src/routes/create-trip.js";
+import { confirmTrip } from "./routes/confirm-trip.js";
+
 const app = fastify();
 
-app.get("/cadastrar", async () => {
-  await prisma.trips.create({
-    data: {
-      destination: "São Paulo",
-      starts_at: new Date(),
-      ends_at: new Date(),
-    },
-  });
-  return "registro cadastrado com sucesso!";
+//Front http://localhost:3050
+app.register(cors, {
+  origin: true,
 });
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
 
-app.get("/listar", async () => {
-  return await prisma.trips.findMany();
-});
-app.listen({ port: 4444 }).then(() => {
-  console.log("Server rodando");
-});
+app.register(createTrip);
+app.register(confirmTrip);
+// Atualizando para usar o objeto de opções
+app
+  .listen({ port: 3090, host: "0.0.0.0" })
+  .then(() => {
+    console.log("Server rodando");
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
